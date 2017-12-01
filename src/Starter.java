cf45bc210212f5670baa38a37efc0deaccec4296
Oskar Polak <oskarpolak@vp.pl>
2017-12-01 11:56:12 +0100

    asd

--- src/Starter.java
+++
@@ -1,9 +0,0 @@
-import java.net.HttpURLConnection;
-
-public class Starter {
-    public static void main(String[] args) {
-        HttpService httpService = HttpService.getService();
-        System.out.println(
-                httpService.makeHttpCall("https://maps.googleapis.com/maps/api/place/textsearch/json?query=Gdynia+pizzeria&radius=500&types=food&name=cruise&key=AIzaSyCqBqE4ecsAIlPE-hHXnakh4Ykde3BCs6I"));
-    }
-}
